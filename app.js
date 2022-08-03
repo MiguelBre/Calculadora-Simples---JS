ee0d8cc1e1e064cdaac4267dd8e3014dd4408261
MiguelBre <migbress12@gmail.com>
2022-08-03 09:27:51 -0300

    Finalização sem contra- ERROS

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 'use strict'
 
-function juntarNome (){
-    const nome = document.getElementById('nome').value;
-    const sobrenome = document.getElementById('sobrenome').value;
-    const resultado = document.getElementById('resultado');
+var resultado = document.getElementById('resultado')
 
-    const nomeCompleto = `Seu nome completo é: ${nome} ${sobrenome}`;
+function somar (){
+    const valor1 = document.getElementById('numero1').value;
 
-    resultado.textContent = nomeCompleto;
+    const valor2 = document.getElementById('numero2').value;
+
+
+    const soma = parseFloat(valor1) + parseFloat(valor2);
+
+    const total = 'A soma é: ' + soma;
+
+    resultado.textContent = total;
 }
 
-document.getElementById('juntar').addEventListener('click', juntarNome);
+
+function subtrair (){
+    const valor1 = document.getElementById('numero1').value;
+
+    const valor2 = document.getElementById('numero2').value;
+
+
+    const subtracao = parseFloat(valor1) - parseFloat(valor2);
+
+    const total = 'A subtração é: ' + subtracao;
+
+    resultado.textContent = total;
+}
+
+
+function multiplicar (){
+    const valor1 = document.getElementById('numero1').value;
+
+    const valor2 = document.getElementById('numero2').value;
+
+
+    const multiplicacao = parseFloat(valor1) * parseFloat(valor2);
+
+    const total = 'A multiplicação é: ' + multiplicacao;
+
+    resultado.textContent = total;
+}
+
+
+function dividir (){
+    const valor1 = document.getElementById('numero1').value;
+
+    const valor2 = document.getElementById('numero2').value;
+
+
+    const divisao = parseFloat(valor1) / parseFloat(valor2);
+
+    const total = 'A divisão é: ' + divisao;
+
+    resultado.textContent = total;
+}
+
+document.getElementById('soma').addEventListener('click', somar);
+document.getElementById('subtrair').addEventListener('click', subtrair);
+document.getElementById('multiplicar').addEventListener('click', multiplicar);
+document.getElementById('dividir').addEventListener('click', dividir);
